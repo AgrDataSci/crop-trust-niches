7be2431f81a0ccb1ec82c3e8827f49534adbd3fe
kauedesousa <kaue.desousa@inn.no>
2024-01-30 14:21:45 +0100

    script-process-ecocrop

---
+++ 03-ecocrop-prossessing-layers.R
@@ -0,0 +1,103 @@
+library("geodata")
+library("terra")
+library("janitor")
+
+here = "output/ecocrop-raw/"
+
+output = "output/ecocrop/"
+
+dir.create(output, recursive = TRUE, showWarnings = FALSE)
+
+# read file with crop parameters
+plant_spp = read.csv("data/calib-eco-pars.csv")
+
+plant_spp$name2 = make_clean_names(plant_spp$NAME)
+
+# read with file with models to run 
+model_runs = read.csv("data/worldclim-cmip6-model-runs.csv")
+
+keep = !duplicated(paste0(model_runs$V2, model_runs$V3))
+
+model_runs = model_runs[keep, c("V2", "V3")]
+
+# ........................................
+# ........................................
+# Average rasters by SSPs #####
+# this will run over GCMs to get the average and over crops 
+crop_spp = unique(plant_spp$crop)
+
+gcm = paste(model_runs$V2, model_runs$V3, sep = "-")
+
+for (i in seq_along(plant_spp$name2)) {
+  
+  index = plant_spp$name2[i]
+  
+  files = list.files(here, pattern = index, full.names = TRUE)
+  
+  for (j in seq_along(gcm)) {
+    
+    f = grep(gcm[j], files)
+    
+    f = files[f]
+    
+    r = rast(f)
+    
+    r = mean(r)
+    
+    writeRaster(r, filename = paste0(here, index, "-SSP-", gcm[j], ".tif"))
+    
+  }
+  
+}
+
+# ........................................
+# ........................................
+# Max value per SPAM crop ####
+# get the max value in each cell combining ecocrop plant species
+# into a single SPAM crop 
+spam = unique(plant_spp$SPAM_Name)
+
+ssp = c("current", paste0("SSP-", gcm))
+
+for (i in seq_along(spam)) {
+  
+  index = plant_spp[plant_spp$SPAM_Name == spam[i], "name2"]
+  
+  index1 = paste(index, collapse = "|")
+  
+  files = list.files(here, pattern = index1, full.names = TRUE)
+  
+  for (j in seq_along(ssp)) {
+    
+    print(paste(spam[i], ssp[j]))
+    
+    f = grep(ssp[j], files)
+    
+    f = files[f]
+    
+    r = rast(f)
+    
+    r = max(r)
+    
+    names(r) = gsub(" |-", "", paste(spam[i], ssp[j]))
+    
+    writeRaster(r, filename = paste0(output, spam[i], "-", ssp[j], ".tif"), overwrite = TRUE)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script/02-ecocrop-modelling.R
+++ script/02-ecocrop-modelling.R
@@ -1,17 +1,32 @@
 library("geodata")
 library("Recocrop")
 library("terra")
-library("data.table")
+library("janitor")
+
+outputdir = "output/ecocrop-raw"
+
+dir.create(outputdir, showWarnings = FALSE, recursive = TRUE)
 
 # https://reagro.org/blocks/when/ecocrop.html
 wcpath = "data/wc2.1-global"
 
 res = 5
 
+# read file with crop parameters
+plant_spp = read.csv("data/calib-eco-pars.csv")
+
+plant_spp$name2 = make_clean_names(plant_spp$NAME)
+
+# read with file with models to run 
+model_runs = read.csv("data/worldclim-cmip6-model-runs.csv")
+
+keep = !duplicated(paste0(model_runs$V1, model_runs$V2, model_runs$V3))
+
+model_runs = model_runs[keep, ]
+
+# read current climate data
 rain = worldclim_global("prec", res = res, wcpath)
 names(rain) = paste0("rain", 1:length(names(rain)))
-
-plot(rain)
 
 tmin = worldclim_global("tmin", res = res, wcpath)
 names(tmin) = paste0("tmin", 1:length(names(tmin)))
@@ -22,111 +37,66 @@ names(tmax) = paste0("tmax", 1:length(names(tmax)))
 tavg = mosaic(tmin, tmax, fun = "mean")
 names(tavg) = paste0("tavg", 1:length(names(tavg)))
 
-crop = ecocropPars("Wheat, common")
 
-crop
+for (p in seq_along(plant_spp$NAME)) {
+  
+  print(plant_spp$NAME[p])
+  
+  crop = ecocropPars(plant_spp$NAME[p])
+  
+  m = ecocrop(crop)
+  
+  control(m, get_max = TRUE)
+  
+  plant = predict(m, 
+                  tavg = tavg, 
+                  prec = rain, 
+                  filename = paste0(outputdir, "/", plant_spp$name2[p], "-current.tif"),
+                  overwrite = TRUE)
+  
+  # now run over climate scenarios 
+  for (i in seq_len(nrow(model_runs))) {
+    
+    print(paste(plant_spp$NAME[p], paste(model_runs[i, 1:3], collapse = " - ")))
+    
+    rain_future = cmip6_world(model = model_runs[i, 1],
+                              ssp = model_runs[i, 2],
+                              time = model_runs[i, 3],
+                              var = "prec",
+                              res = res, 
+                              path = wcpath)
+    names(rain_future) = paste0("rain", 1:length(names(rain_future)))
+    
+    tmax_future = cmip6_world(model = model_runs[i, 1],
+                              ssp = model_runs[i, 2],
+                              time = model_runs[i, 3],
+                              var = "tmax",
+                              res = res, 
+                              path = wcpath)
+    names(tmax_future) = paste0("tmax", 1:length(names(tmax_future)))
+    
+    tmin_future = cmip6_world(model = model_runs[i, 1],
+                              ssp = model_runs[i, 2],
+                              time = model_runs[i, 3],
+                              var = "tmin",
+                              res = res, 
+                              path = wcpath)
+    names(tmin_future) = paste0("tmin", 1:length(names(tmin_future)))
+    
+    tavg_future = mosaic(tmin_future, tmax_future, fun = "mean")
+    names(tavg_future) = paste0("tavg", 1:length(names(tavg_future)))
+    
+    plant_future = predict(m, 
+                           tavg = tavg_future, 
+                           prec = rain_future,
+                           filename = paste0(outputdir, "/", 
+                                             plant_spp$name2[p], 
+                                             "-", paste(model_runs[i, 1:3], collapse = "-"), 
+                                             ".tif"),
+                           overwrite = TRUE)
+    
+  }
+  
+}
 
-m = ecocrop(crop)
-m
-plant = predict(m, prec = rain, tavg = tavg)
-p = classify(plant > 0, cbind(0,NA)) * 1:12
-pm = median(p, na.rm = TRUE)
-hv = pm + m$duration
-hv = ifel(hv > 12, hv - 12, hv)
-plot(hv)
 
-
-control(m, which_max=TRUE)
-mplant = predict(m, prec = rain, tavg = tavg)
-mhv = mplant + m$duration
-mhv = ifel(mhv > 12, mhv - 12, mhv)
-plot(mhv)
-
-# crop parameters obtained from 
-# https://github.com/AramburuMerlos/globcropdiv
-# http://doi.org/10.1088/1748-9326/ac62ab
-# dpars = fread("data/calib-eco-pars.csv")
-# crops = unique(dpars$crop)
-# 
-# rpar = c("RMIN", "ROPMN", "ROPMX", "RMAX")
-# phpar = c("PHMIN", "PHOPMN", "PHOPMX", "PHMAX")
-# tpar = c("TMIN","TOPMN","TOPMX","TMAX")
-# mrpar = paste0(rpar, "_M")
-# 
-# 
-# 
-# for(i in 1:length(crops)){
-#    
-# spp = which(dpars$crop == crops[i])
-# score_spp = paste0("score_", spp) 
-#   
-#   # run for each species within crop category
-#   for(j in 1:length(spp)){
-#     # crop species parameters
-#     crop_pars = cbind(
-#       duration = c(dpars[spp[j], duration], NA, NA, NA),
-#       ktmp = dpars[spp[j], KTMP] + c(-1, +1, NA, NA),
-#       tavg = unlist(dpars[spp[j], ..tpar], use.names = FALSE),
-#       prec = unlist(dpars[spp[j], ..mrpar], use.names = FALSE),
-#       ph = unlist(dpars[spp[j], ..phpar], use.names = FALSE),
-#       anpr = unlist(dpars[spp[j], ..rpar], use.names = FALSE)
-#     )
-#     
-#     foo = list(name = dpars[spp[j], NAME], 
-#                 parameters = crop_pars)
-#     # rainfed 
-#     m = ecocrop(foo)
-#     
-#     control(m, get_max = TRUE)
-#     
-#     plant = predict(m, prec = rain, tavg = tavg)
-#     
-#     p = classify(plant > 0, cbind(0,NA)) * 1:12
-#     pm = median(p, na.rm = TRUE)
-#     hv = pm + m$duration
-#     hv = ifel(hv > 12, hv - 12, hv)
-#     plot(hv)
-#     
-#     
-#     
-#     dynamicPredictors(m) = cbind(
-#       ktmp = as.vector(t(d[, ..tmin])),
-#       tavg = as.vector(t(d[, ..tavg])),
-#       prec = as.vector(t(d[, ..prec]))
-#     ) 
-#     staticPredictors(m) = cbind(
-#       ph = d$ph,
-#       anpr = d$anpr
-#     )
-#     d[, rfp:= run(m)]
-#     
-#     # irrigated
-#     m = ecocrop(foo)
-#     control(m, get_max=TRUE)
-#     dynamicPredictors(m) = cbind(
-#       ktmp = as.vector(t(d[, ..tmin])),
-#       tavg = as.vector(t(d[, ..tavg]))
-#     ) 
-#     staticPredictors(m) = cbind(ph = d$ph)
-#     d[, irp:= run(m)]
-#     
-#     # species prediction
-#     d[, (score_spp[j]):= rfp * (1 - irri/100) + irp * irri/100]
-#     d[, c("rfp", "irp"):= NULL]
-#   }
-#   # get maximum score for all species within crop i
-#   d[, (crops[i]):= do.call(pmax, .SD), .SDcols = score_spp]
-#   d[, (score_spp):= NULL]
-#   
-#   # write values to spatRast and disk
-#   r = rast(totcl)
-#   v = rep(NA_real_, ncell(r))
-#   v[d$cell] = d[[crops[i]]]
-#   values(r) = v
-#   writeRaster(r, filename = file.path(outdir, paste0(crops[i], ".tif")),
-#               overwrite = T, 
-#               wopt = list(names = crops[i], filetype = "GTiff",
-#                           gdal=c("COMPRESS=Deflate","PREDICTOR=1","ZLEVEL=6"))
-#   )
-# }
-# 
